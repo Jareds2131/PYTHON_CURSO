609bc094439d51cca9c3124c31611d617868134f
Jareds2131 <103007036+Jareds2131@users.noreply.github.com>
2022-04-05 14:57:58 -0500

    Repasos

---
+++ Operacion_con_variables.py
@@ -0,0 +1,8 @@
+n1 = int(input("Esbribe un numero: "))
+n2 = int(input("Ingrese el numero 2: "))
+c = int(input("Ingresa numero 3: "))
+nr = (n1 + n2 )/(c)
+
+print("El la suma es: ", nr)
+
+ 
---
+++ Variables.py
@@ -0,0 +1,5 @@
+nom = input("INGRESA TU NOMBRE: ")
+ape = input("INGRESA TU APELLIDO: ")
+ape1 = input("ONGRESA TU SEGUNNDO APELLIDO: ")
+
+print(nom + " " + ape + " " + ape1);
