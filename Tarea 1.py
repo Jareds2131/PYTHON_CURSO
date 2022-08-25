59fe839e979e1ee402be0b716ec18d94d8e66751
Jareds2131 <103007036+Jareds2131@users.noreply.github.com>
2022-08-24 19:46:12 -0500

    Add files via upload

---
+++ Tarea 1.py
@@ -0,0 +1,10 @@
+Num1 = float(input("ESCRIBE NUMERO 1: "))
+Num2 = float(input("ESCRIBE NUMERO 2: "))
+
+Res = Num1 * Num2
+
+if (Res >= 0):
+    print(Res , "ES NUMEOR POSITIVO")
+
+else:
+    print(Res, "NUMERO NEGATIVO")
